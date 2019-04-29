285a209fb17230b45844c2f22e433f8c3cbf6bf3
Ammar Yasser <plus.ammaryasser@gmail.com>
2019-04-29 16:08:50 +0200

    Initial commit

---
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -0,0 +1,9 @@
+package mastermind
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
+    val commonLetteers = "ABCDEF".sumBy { ch -> Math.min(secret.count() { it == ch }, guess.count() { it == ch }) }
+    return Evaluation(rightPositions, commonLetteers - rightPositions)
+}
